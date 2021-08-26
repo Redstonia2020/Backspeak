05ccb682e4f854c20bbcde6b2adda8d23371eea4
Redstonia <redstonia1234@gmail.com>
2021-08-27 10:04:37 +1200

    Add project files.

---
+++ Backspeak/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Backspeak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    char[] charArray = Console.ReadLine().ToCharArray();
+                    Array.Reverse(charArray);
+                    Console.WriteLine(new string(charArray));
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine("We seem to have encountered a problem.");
+                    Console.WriteLine(e);
+                    Console.WriteLine("-------------------------------------------");
+                }
+            }
+        }
+    }
+}
